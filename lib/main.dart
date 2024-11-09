4fb826ecc7273a631150e09ea6900aa0c8a413c1
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-11-09 13:03:46 +0200

    Add internationalization support with Intl package
    This commit adds support for internationalization (i18n) to the Flutter app using the Intl package.
    
    The changes include:
    
    1. Added the required files and directories for internationalization:
       - `lib/generated/intl/messages_all.dart`: Generated file for message lookup.
       - `lib/generated/intl/messages_en.dart`: Generated file for English messages.
       - `lib/generated/l10n.dart`: Generated file for the S class, which provides access to the localized strings.
       - `lib/l10n/intl_en.arb`: The ARB file for English locale.
    
    2. Updated the `main.dart` file to initialize the localization delegates and supported locales.
    
    3. Added the `flutter_localizations` package as a dependency in the `pubspec.yaml` file.
    
    4. Enabled the Flutter Intl plugin in the `pubspec.yaml` file.
    
    This change lays the foundation for supporting multiple languages in the future and makes the app more accessible to users from different regions.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'features/auth/logic/login_provider.dart';
-import 'features/auth/ui/views/login_view.dart';
-import 'features/movies/ui/views/movies_view.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
 import 'core/app/app_router.dart';
 import 'core/app/di.dart';
+import 'features/auth/logic/login_provider.dart';
+import 'features/auth/ui/views/login_view.dart';
+import 'features/movies/ui/views/movies_view.dart';
+import 'generated/l10n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,6 +25,13 @@ class MainApp extends StatelessWidget {
     return ChangeNotifierProvider.value(
       value: getIt<LoginProvider>(),
       child: MaterialApp(
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
         debugShowCheckedModeBanner: false,
         theme: ThemeData.dark(useMaterial3: true),
         onGenerateRoute: AppRouter.generateRoute,
